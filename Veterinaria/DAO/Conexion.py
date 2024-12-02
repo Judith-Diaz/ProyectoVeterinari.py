e42f24731b4cc47ff5809497878ce6b475ca6a88
Benja-099 <benjaminduran612@gmail.com>
2024-12-02 02:30:59 -0300

    carpetas de clases y clase conexion

---
+++ Veterinaria/DAO/Conexion.py
@@ -0,0 +1,23 @@
+import sqlite3;
+class Conexion:
+    def conectar(self):
+      conexion = sqlite3.connect("C:\Users\matias duran\Desktop\VeterianrioCusoPython\ProyectoVeterinari.py\Veterinaria\BDProyectoPy.db")
+      return conexion
+    
+def cargar_materias():
+    try:
+        cone = Conexion()
+        conexion = cone.conectar()
+        cur = conexion.cursor()
+        sql = "SELECT * FROM PracticaMedica;"
+        cur.execute(sql)
+        resultado = cur.fetchall()
+        print(resultado)
+        resultado2 = []
+        for r in resultado:
+            resultado2.append({"IdPracticaMe_pra": r[0], "decripcion_pra": r[1]})
+        return resultado2
+    finally:
+        conexion.close()
+
+print(cargar_materias())
--- Veterinaria/Vistas/MenuPrincipal.py
+++ Veterinaria/Vistas/MenuPrincipal.py
@@ -9,7 +9,7 @@ from Presentacion import Ui_MainWindow  # Asegúrate de que el nombre sea correc
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)  # Configura la interfaz gráfica en este QMainWindow
+        self.setupUi(self)  
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
