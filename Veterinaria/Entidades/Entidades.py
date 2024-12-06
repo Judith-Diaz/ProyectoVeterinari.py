adafab53adc6b9bb8d6d4f7fc072b4ee071e7fe2
Xuxa <105894846+Judith-Diaz@users.noreply.github.com>
2024-12-06 00:07:05 -0300

    entidades

---
+++ Veterinaria/Entidades/Entidades.py
@@ -0,0 +1,21 @@
+class PracticaMedica:
+    def __init__(self,Id,Descripcion):
+        self._Id = Id      
+        self._Descripcion = Descripcion
+
+
+
+        def Id(self):
+            return self._Id
+
+ 
+        def Id(self, value):
+            self._Id = value
+
+ 
+        def Descripcion(self):
+            return self._Descripcion
+
+        def Descripcion(self, value):
+            self._Descripcion = value
+
